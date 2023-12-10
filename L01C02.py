52804328d73d18c15ea43881dc49668a12275997
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2023-12-10 14:50:21 -0600

    Create L01C02.py

---
+++ L01C02.py
@@ -0,0 +1,47 @@
+# A variable is just a box with a label on it that can hold something.
+
+# What you put in the box is up to you, but no matter what is inside,
+# you can refer to whatever is there by the label on the box.
+
+stuff = "hello"
+print(stuff)
+
+stuff = "world!"
+print(stuff)
+
+# See? The contents of the variable 'stuff' changed, but we can still
+# refer to whatever is there by the label on the box.
+
+# In programming, there are different 'types' of information.
+# Learn the ones below.
+# String - A string is text. Strings usually have to be wrapped
+# in quotation marks.
+# Integer - An integer is a whole number. Integers shouldn't have
+# quotation marks.
+# Float - A float is a number with a decimal point. Floats shouldn't
+# have quotation marks.
+# Boolean - A boolean value can either be True or False. Boolean
+# values shouldn't have quotes.
+
+# Python automatically changes the type of data a variable can hold
+# depending on what you try to put into it.
+# Sometimes it won't be able to do it automatically, and you'll have
+# to convert it yourself.
+
+# CHALLENGE 1: First create a variable called aString and assign it
+# the following text: Testing
+# Remember the format to assign data to a variable
+# ie: variableName = "SomeValue"
+
+# CHALLENGE 2: Next create a variable called anInt and assign it the
+# following number: 899
+# Remember, you don't need quotes for numbers!
+
+# CHALLENGE 3: Next create a variable called aFloat and assign it the
+# following number: 12.55
+
+# CHALLENGE 4: Next create a variable called aBool and assign it the
+# value False.
+
+# CHALLENGE 5: Print the variables out in order of the challenge
+# number. i.e Challenge 1 first, then Challenge 2, and so on.
