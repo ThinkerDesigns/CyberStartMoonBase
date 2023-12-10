e9b190b68466dcf6e1a7c84aca522aef16345159
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2023-12-10 14:51:18 -0600

    Create L01C04.py

---
+++ L01C04.py
@@ -0,0 +1,38 @@
+# You can do maths in programming too...
+
+addition = 5 + 5
+print(addition) # This will print 10.
+
+multiplication = 5 * 5
+print(multiplication) # This will print 25.
+
+division = 5 / 5
+print(division) # This will print 1.
+
+# You can also do maths without saving the result in a variable.
+
+print(100 - 42)
+
+# Here are some mathematical operators you should know about:
+# Addition [+], as in 5 + 5 = 10
+# Subtraction [-], as in 10 - 2 = 8
+# Multiplication [*], as in 5 * 5 = 25
+# Division [/], as in 10 / 2 = 5
+# Power Of [**] as in 2 ** 3 = 8
+
+# CHALLENGE 1: Multiply 80 by 512 and save it to a variable called ch1.
+
+# CHALLENGE 2: Add 1024 to 768 and save it to a variable called ch2.
+
+# CHALLENGE 3: Do 40 minus 100 and save it to a variable called ch3.
+
+# CHALLENGE 4: Divide 33 by 6 and save it to a variable called ch4.
+
+# CHALLENGE 5: Do 2 to the power of 6 and save it to a variable called ch5.
+
+# CHALLENGE 6: Save 0.33 to a variable called ch6_1, and save 5 to a
+#              variable called ch6_2. Multiply ch6_1 with ch6_2 and save
+#              the result to a variable called ch6.
+
+# CHALLENGE 7: Print out the following variables in order:
+#              ch1, ch2, ch3, ch4, ch5, ch6
