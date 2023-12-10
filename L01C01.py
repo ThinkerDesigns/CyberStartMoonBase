e133d406a90736d3b4dc11d838f2ba092156dbfe
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2023-12-10 14:48:11 -0600

    Create L01C01.py

---
+++ L01C01.py
@@ -0,0 +1,19 @@
+# This is a code comment
+# Anything with a '#' before it is considered a comment and ignored.
+# You can use these for writing notes about what your code is doing.
+# This is so you can understand it more easily if you
+# come back to it after some time.
+
+# In Python, you can output text to the screen using the print function.
+# The function is print("text")
+
+print("text")
+
+# You can also print numbers out. With numbers, you don't have to
+# put them in quotation marks.
+
+print(42)
+
+# CHALLENGE 1: First print out the text: Hello, World
+
+# CHALLENGE 2: Next print out the numbers: 1337
